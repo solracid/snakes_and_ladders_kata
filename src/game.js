3d1f82282d5b24a5fb3343f5ef6ed25769b18686
solracid <carlos.garces.garcia@gmail.com>
2019-04-25 15:46:06 +0200

    snakes and ladders definition

---
+++ src/game.js
@@ -0,0 +1,9 @@
+function game(){
+    this.players = 1;
+    this.snakesAndLadders = {
+        start: [2, 12],
+        end:  [12, 2]
+    }
+}
+
+module.exports = game;
--- src/player.js
+++ src/player.js
@@ -1,14 +1,19 @@
-function Player() {
+const game = require('./game');
+
+var Game = new game;
+
+function player() {
     this.position = 1;
     this.win = false;
-    this.move = function(squares){
-        if (this.position + squares <= 100){
-            return this.position += squares;
-        } else if (this.position + squares > 100){
-            return this.position
-        } else if (this.position + squares == 100){
+    this.move = function(roll){
+        let moveTo = roll + this.position;
+        if (Game.snakesAndLadders.start.indexOf(moveTo) > -1) {
+            this.position = Game.snakesAndLadders.end[Game.snakesAndLadders.start.indexOf(moveTo)];
+        } else if (moveTo <= 100) {
+            return this.position += roll;
+        } else if (moveTo == 100){
             this.position = 100
-            return this.win = true;;
+            return this.win = true;
         }
     };
     this.rollDie = function(){
@@ -16,4 +21,4 @@ function Player() {
     };
 };
 
-module.exports = Player;
+module.exports = player;
