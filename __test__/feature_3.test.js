77703776ea2c1783ec6a7083b2718075111fd407
solracid <solracid@users.noreply.github.com>
2019-05-08 17:58:36 +0200

    Feature 3 Test created

---
+++ __test__/feature_3.test.js
@@ -0,0 +1,16 @@
+var Player = require('../src/player');
+var Game = require('../src/player');
+
+describe('Snakes and Ladders - Feature 3', function(){
+
+  beforeAll(function(){
+    let game = new Game();
+    game.players = 2
+  });
+
+  test('Players must roll a dice', function(){
+    
+  });
+
+
+});
