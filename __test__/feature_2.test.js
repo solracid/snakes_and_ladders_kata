9af3c3973189b153d353bbb64cb8799796a1ea76
solracid <solracid@users.noreply.github.com>
2019-05-08 17:18:19 +0200

    Test Feature 2

--- __test__/feature_2.test.js
+++ __test__/feature_2.test.js
@@ -1,11 +1,14 @@
 const Player = require('../src/player');
+const Game   = require('../src/game');
 
 describe('Snakes and Ladders - Feature 2', function() {
 
     let player
+    let game
 
-    beforeEach(function(){
-        player = new Player()
+    beforeAll(function(){
+        player = new Player();
+        game = new Game();
     })
     
     describe('Snakes Go Down, Not Up', function(){
@@ -16,8 +19,8 @@ describe('Snakes and Ladders - Feature 2', function() {
             expect(player.position).toEqual(2);
         })
         test('Snakes go Down, not Up', function(){
-            player.position = 1
-            player.move(1)
+            player.position = 2
+            expect(typeof game.snakes.get(2)).toBeUndefined();
             expect(player.position).toEqual(2);
         })
         test('Ladders go Up', function(){
@@ -26,8 +29,8 @@ describe('Snakes and Ladders - Feature 2', function() {
             expect(player.position).toEqual(12);
         })
         test('Ladders go Up, not Down', function(){
-            player.position = 8
-            player.move(4)
+            player.position = 12
+            expect(typeof game.ladders.get(12)).toBeUndefined();
             expect(player.position).toEqual(12);
         })
     })
