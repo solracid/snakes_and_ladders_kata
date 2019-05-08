0e10a8eba4679e24183bea058d500b0b81c07dbb
solracid <solracid@users.noreply.github.com>
2019-05-08 18:02:39 +0200

    Set the number of players in the game

--- src/game.js
+++ src/game.js
@@ -1,7 +1,15 @@
+const Player = require('./player');
+
 function game(){
-    this.players = 1;
+    this.players;
     this.snakes = new Map([[12,2]]);
     this.ladders = new Map([2,12]);
+    this.setNumberOfPlayers = function(num){
+        let i
+        for(i = 0; i < num; ++i) {
+            this.players[i] = new Player();
+        }
+    };
 };
 
 module.exports = game;
