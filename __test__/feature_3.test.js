e8d5e3c6ec8753eed82589230b2510350861d01d
solracid <solracid@users.noreply.github.com>
2019-05-10 18:01:28 +0200

    Update feature_3.test.js

--- __test__/feature_3.test.js
+++ __test__/feature_3.test.js
@@ -5,12 +5,33 @@ describe('Snakes and Ladders - Feature 3', function(){
 
   beforeAll(function(){
     let game = new Game();
-    game.players = 2
-  });
-
-  test('Players must roll a dice', function(){
+    game.setNumberOfPlayers(2);
     
+    let mockedDie1 = jest.spyOn(game.players[0], 'rollDie')
+    let mockedDie2 = jest.spyOn(game.players[0], 'rollDie')
   });
 
+  test('Players must roll a dice to determine its play order', function(){
+    expect(game.players.size).Equals(2);
+    expect(typeof game.players[0].playOrder).toBe("number");
+    expect(typeof game.players[1].playOrder).toBe("number");
+    expect(game.players[0].playOrder).not.toBe(game.players[1].playOrder);
+  });
+
+  test('Player 1 rolls first', function(){
+    mockedDie1.mockImplementation(function(){
+        return 6
+    });
+    mockedDie2.mockImplementation(function(){
+        return 1
+    });
+    
+    
+
+      
+
+    mockedDie1.mockRestore()
+    mockedDie2.mockRestore()
+  });
 
 });
