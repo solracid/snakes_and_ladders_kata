67af88c071a7535eb27303eaf0573ddca31241e4
solracid <solracid@users.noreply.github.com>
2019-05-13 14:25:53 +0200

    The die is part of the board game

--- src/board.js
+++ src/board.js
@@ -1,6 +1,9 @@
 var board = {
     snakes : new Map([[12,2]]),
-    ladders : new Map([[2,12]])
+    ladders : new Map([[2,12]]),
+    die: function(){
+        return Math.floor(Math.random() * 6) + 1
+    }
 };
 
 module.exports = board;
